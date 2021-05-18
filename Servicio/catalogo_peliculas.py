58f38727a988e5b3e46f3607d467dc2289f2f043
jesus <chucho.cisneros@hotmail.com>
2021-05-18 07:57:53 -0500

    Catalogo de peliculas

--- Servicio/catalogo_peliculas.py
+++ Servicio/catalogo_peliculas.py
@@ -2,36 +2,35 @@ import os
 
 class CatalogoPeliculas:
     
-    ruta_archivo = "CATALOGO_pelicula.txt"
+    ruta_archivo = "peliculas.txt"
     
-    @staticmethod           
-    
+    @staticmethod
     def agregar_pelicula(pelicula):
         try:
+            # "a" - modo append
             archivo = open(CatalogoPeliculas.ruta_archivo, "a")
-            archivo.write(pelicula.__str__()+"\n")
+            archivo.write(pelicula.__str__() + "\n")
         except Exception as e:
-            print("Ocurrio una excepcion al agregar: ", e)
+            print("Ocurrió una excepción al agregar: " , e)
         finally:
-            archivo.close()
-
+            archivo.close()    
+    
     @staticmethod
     def listar_peliculas():
         try:
             archivo = open(CatalogoPeliculas.ruta_archivo, "r")
             print("Catálogo de Películas:")
-            print(archivo.read())
+            print(archivo.read()) 
         except Exception as e:
             print("Ocurrió un error al listar películas: ", e)
         finally:
-            archivo.close()
-
-
-    
+            archivo.close()     
+            
     @staticmethod
     def eliminar():
-        try: 
+        try:
             os.remove(CatalogoPeliculas.ruta_archivo)
-            print("Archivo eliminado: ",CatalogoPeliculas.ruta_archivo)
+            print("archivo eliminado: ", CatalogoPeliculas.ruta_archivo)
         except Exception as e:
-             print("Ocurrio un error al eliminar peliculas: ", e)
+            print("Ocurrió un error al eliminar películas:", e)
+                          
